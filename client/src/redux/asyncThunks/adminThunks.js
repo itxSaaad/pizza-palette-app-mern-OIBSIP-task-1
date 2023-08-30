edf203349b6261e75506aab3e53537f73c2a5e41
itxSaaad <saadstudent.cs@gmail.com>
2023-08-30 18:02:04 +0500

    Added: All Admin Thunks and Slices

--- client/src/redux/asyncThunks/adminThunks.js
+++ client/src/redux/asyncThunks/adminThunks.js
@@ -62,3 +62,212 @@ export const registerAdmin = createAsyncThunk(
     }
   }
 );
+
+// Admin Update Profile
+export const updateAdminProfile = createAsyncThunk(
+  'admin/updateProfile',
+  async (
+    { name, email, password, confirmPassword },
+    { rejectWithValue, getState }
+  ) => {
+    try {
+      const {
+        admin: { adminUserInfo },
+      } = getState();
+
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+
+          Authorization: `Bearer ${adminUserInfo.token}`,
+        },
+      };
+
+      const { data } = await axios.put(
+        `${import.meta.env.VITE_SERVER_URL}/admin/profile`,
+        { name, email, password, confirmPassword },
+        config
+      );
+
+      return data;
+    } catch (error) {
+      return rejectWithValue({
+        status: error.response && error.response.status,
+        message:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      });
+    }
+  }
+);
+
+// Admin User Details
+export const getAdminUserDetails = createAsyncThunk(
+  'admin/userDetails',
+  async (_, { rejectWithValue, getState }) => {
+    try {
+      const {
+        admin: { adminUserInfo },
+      } = getState();
+
+      const config = {
+        headers: {
+          Authorization: `Bearer ${adminUserInfo.token}`,
+        },
+      };
+
+      const { data } = await axios.get(
+        `${import.meta.env.VITE_SERVER_URL}/admin/profile`,
+        config
+      );
+
+      return data;
+    } catch (error) {
+      return rejectWithValue({
+        status: error.response && error.response.status,
+        message:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      });
+    }
+  }
+);
+
+// Admin List Users
+export const listAdminUsers = createAsyncThunk(
+  'admin/listUsers',
+  async (_, { rejectWithValue, getState }) => {
+    try {
+      const {
+        admin: { adminUserInfo },
+      } = getState();
+
+      const config = {
+        headers: {
+          Authorization: `Bearer ${adminUserInfo.token}`,
+        },
+      };
+
+      const { data } = await axios.get(
+        `${import.meta.env.VITE_SERVER_URL}/admin`,
+        config
+      );
+
+      return data;
+    } catch (error) {
+      return rejectWithValue({
+        status: error.response && error.response.status,
+        message:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      });
+    }
+  }
+);
+
+// Admin USer Details By ID
+export const getAdminUserDetailsById = createAsyncThunk(
+  'admin/userDetailsById',
+  async (id, { rejectWithValue, getState }) => {
+    try {
+      const {
+        admin: { adminUserInfo },
+      } = getState();
+
+      const config = {
+        headers: {
+          Authorization: `Bearer ${adminUserInfo.token}`,
+        },
+      };
+
+      const { data } = await axios.get(
+        `${import.meta.env.VITE_SERVER_URL}/admin/${id}`,
+        config
+      );
+
+      return data;
+    } catch (error) {
+      return rejectWithValue({
+        status: error.response && error.response.status,
+        message:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      });
+    }
+  }
+);
+
+// Admin Update User By ID
+export const updateAdminUserById = createAsyncThunk(
+  'admin/updateUserById',
+  async (
+    { id, name, email, role, permissions, isApproved },
+    { rejectWithValue, getState }
+  ) => {
+    try {
+      const {
+        admin: { adminUserInfo },
+      } = getState();
+
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${adminUserInfo.token}`,
+        },
+      };
+
+      const { data } = await axios.put(
+        `${import.meta.env.VITE_SERVER_URL}/admin/${id}`,
+        { name, email, role, permissions, isApproved },
+        config
+      );
+
+      return data;
+    } catch (error) {
+      return rejectWithValue({
+        status: error.response && error.response.status,
+        message:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      });
+    }
+  }
+);
+
+// Admin Delete User By ID
+export const deleteAdminUserById = createAsyncThunk(
+  'admin/deleteUserById',
+  async (id, { rejectWithValue, getState }) => {
+    try {
+      const {
+        admin: { adminUserInfo },
+      } = getState();
+
+      const config = {
+        headers: {
+          Authorization: `Bearer ${adminUserInfo.token}`,
+        },
+      };
+
+      const { data } = await axios.delete(
+        `${import.meta.env.VITE_SERVER_URL}/admin/${id}`,
+        config
+      );
+
+      return data;
+    } catch (error) {
+      return rejectWithValue({
+        status: error.response && error.response.status,
+        message:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      });
+    }
+  }
+);
--- client/src/redux/slices/adminSlice.js
+++ client/src/redux/slices/adminSlice.js
@@ -1,20 +1,42 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 // Import Async Thunks
-import { loginAdmin, registerAdmin } from '../asyncThunks/adminThunks';
+import {
+  loginAdmin,
+  registerAdmin,
+  listAdminUsers,
+  updateAdminProfile,
+  updateAdminUserById,
+  deleteAdminUserById,
+  getAdminUserDetails,
+  getAdminUserDetailsById,
+} from '../asyncThunks/adminThunks';
 
 // Initial State
 const initialState = {
   adminUserInfo: localStorage.getItem('adminUserInfo')
     ? JSON.parse(localStorage.getItem('adminUserInfo'))
     : null,
+  adminUserDetails: localStorage.getItem('adminUserDetails')
+    ? JSON.parse(localStorage.getItem('adminUserDetails'))
+    : null,
   adminUserList: [],
   adminUserLoginError: null,
   adminUserRegisterError: null,
   adminUserListsError: null,
+  adminUserDetailsError: null,
+  adminUserUpdateProfileError: null,
+  adminUserDetailsByIdError: null,
+  adminUserUpdateProfileByIdError: null,
+  adminUserDeleteByIdError: null,
   adminUserLoginSuccess: false,
   adminUserRegisterSuccess: false,
   adminUserListsSuccess: false,
+  adminUserDetailsSuccess: false,
+  adminUserUpdateProfileSuccess: false,
+  adminUserDetailsByIdSuccess: false,
+  adminUserUpdateProfileByIdSuccess: false,
+  adminUserDeleteByIdSuccess: false,
   loading: false,
 };
 
@@ -64,6 +86,99 @@ const adminSlice = createSlice({
       .addCase(registerAdmin.rejected, (state, action) => {
         state.loading = false;
         state.adminUserRegisterError = action.payload;
+      })
+      .addCase(listAdminUsers.pending, (state) => {
+        state.loading = true;
+        state.adminUserListsError = null;
+        state.adminUserListsSuccess = false;
+      })
+      .addCase(listAdminUsers.fulfilled, (state, action) => {
+        state.loading = false;
+        state.adminUserList = action.payload;
+        state.adminUserListsSuccess = true;
+      })
+      .addCase(listAdminUsers.rejected, (state, action) => {
+        state.loading = false;
+        state.adminUserListsError = action.payload;
+      })
+      .addCase(getAdminUserDetails.pending, (state) => {
+        state.loading = true;
+        state.adminUserDetailsError = null;
+        state.adminUserDetailsSuccess = false;
+      })
+      .addCase(getAdminUserDetails.fulfilled, (state, action) => {
+        state.loading = false;
+        state.adminUserDetails = action.payload;
+        localStorage.setItem(
+          'adminUserDetails',
+          JSON.stringify(action.payload)
+        );
+        state.adminUserDetailsSuccess = true;
+      })
+      .addCase(getAdminUserDetails.rejected, (state, action) => {
+        state.loading = false;
+        state.adminUserDetailsError = action.payload;
+      })
+      .addCase(updateAdminProfile.pending, (state) => {
+        state.loading = true;
+        state.adminUserUpdateProfileError = null;
+        state.adminUserUpdateProfileSuccess = false;
+      })
+      .addCase(updateAdminProfile.fulfilled, (state, action) => {
+        state.loading = false;
+        state.adminUserInfo = action.payload;
+        localStorage.setItem('adminUserInfo', JSON.stringify(action.payload));
+        localStorage.setItem(
+          'adminUserDetails',
+          JSON.stringify(action.payload)
+        );
+        state.adminUserUpdateProfileSuccess = true;
+      })
+      .addCase(updateAdminProfile.rejected, (state, action) => {
+        state.loading = false;
+        state.adminUserUpdateProfileError = action.payload;
+      })
+      .addCase(getAdminUserDetailsById.pending, (state) => {
+        state.loading = true;
+        state.adminUserDetailsByIdError = null;
+        state.adminUserDetailsByIdSuccess = false;
+      })
+      .addCase(getAdminUserDetailsById.fulfilled, (state, action) => {
+        state.loading = false;
+        state.adminUserDetails = action.payload;
+        state.adminUserDetailsByIdSuccess = true;
+      })
+
+      .addCase(getAdminUserDetailsById.rejected, (state, action) => {
+        state.loading = false;
+        state.adminUserDetailsByIdError = action.payload;
+      })
+      .addCase(updateAdminUserById.pending, (state) => {
+        state.loading = true;
+        state.adminUserUpdateProfileByIdError = null;
+        state.adminUserUpdateProfileByIdSuccess = false;
+      })
+      .addCase(updateAdminUserById.fulfilled, (state, action) => {
+        state.loading = false;
+        state.adminUserDetails = action.payload;
+        state.adminUserUpdateProfileByIdSuccess = true;
+      })
+      .addCase(updateAdminUserById.rejected, (state, action) => {
+        state.loading = false;
+        state.adminUserUpdateProfileByIdError = action.payload;
+      })
+      .addCase(deleteAdminUserById.pending, (state) => {
+        state.loading = true;
+        state.adminUserDeleteByIdError = null;
+        state.adminUserDeleteByIdSuccess = false;
+      })
+      .addCase(deleteAdminUserById.fulfilled, (state) => {
+        state.loading = false;
+        state.adminUserDeleteByIdSuccess = true;
+      })
+      .addCase(deleteAdminUserById.rejected, (state, action) => {
+        state.loading = false;
+        state.adminUserDeleteByIdError = action.payload;
       });
   },
 });
