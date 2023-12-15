ac086f3924e2a8417b0d92b72b92d0d8fda567ff
itxSaaad <saadstudent.cs@gmail.com>
2023-12-15 12:02:24 +0500

    Added: Vite PWA Configs

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,71 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'prompt',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Pizza Palette | MERN',
+        short_name: 'Pizza Palette',
+        description:
+          'Order your favorite pizza Now! We deliver the best pizza in town.',
+        icons: [
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: '/apple-touch-icon.png',
+            sizes: '180x180',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: '/favicon-32x32.png',
+            sizes: '32x32',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: '/favicon-16x16.png',
+            sizes: '16x16',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: '/favicon.ico',
+            sizes: '32x32 16x16',
+            type: 'image/x-icon',
+            purpose: 'any',
+          },
+        ],
+        screenshots: [
+          {
+            src: '/images/screen1.png',
+            sizes: '2880x1800',
+            type: 'image/png',
+          },
+        ],
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        orientation: 'portrait',
+      },
+    }),
+  ],
+});
